9621de954ab7ea56f3a415d09e6cfe1472690420
gonzalem4355 <125481986+gonzalem4355@users.noreply.github.com>
2023-02-22 11:05:30 -0500

    M4T1_loops

---
+++ M4T1/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+/*
+CSC 134
+M4T1- Loops
+Michael Gonzalez
+2/22/23
+Purpose of program is to demonstrate various uses of while.
+*/
+
+// define some functions we use in main
+void say_hello(int count) {
+  // repeat count times
+  int i=0; // "index"
+  while (i < count) {
+    cout << "Hello #" << i+1 << endl;
+    i++; // count up
+  }
+  
+  return; // for void functions, return is optional (implied)
+}
+
+int main() {
+  // call a function
+int times = 0;
+cout << "How many times should i say hello?";
+  cin >> times;
+  say_hello(times);
+}
